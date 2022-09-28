34c86c57a0a63d2fe5aaf7ebd37691baf94f8c57
brenoAmodesto <brenoamodesto58@gmail.com>
2022-09-28 05:29:25 -0300

    teste

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "location" {
+  description = "região"
+  type        = string
+  default     = "Brazil South"
+
+}
+
+
+variable "aws_pub_key" {
+  description = "Public key"
+  type = string
+}
+
+
+variable "azure_pub_key" {
+  description = "Public key"
+  type = string
+}
